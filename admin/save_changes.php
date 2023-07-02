4425e337e6c5745e0570b4365371c72887a7fcbc
Opeyemi <opeyemi.sanni180591052@st.lasu.edu.ng>
2023-07-02 18:17:35 +0100

    Almost finished

--- admin/save_changes.php
+++ admin/save_changes.php
@@ -4,8 +4,18 @@ $jsonFile = "../json/blogs.json";
 if (isset($_POST['updatedData'])) {
   $updatedData = $_POST['updatedData'];
 
-  // Save the updated data to the JSON file
-  file_put_contents($jsonFile, $updatedData);
+  // Rearrange the data array based on timestamp
+  $jsonData = json_decode($updatedData, true);
+
+  usort($jsonData, function($a, $b) {
+    $timestampA = DateTime::createFromFormat('d/m/Y, H:i:s', $a['timestamp'])->getTimestamp();
+    $timestampB = DateTime::createFromFormat('d/m/Y, H:i:s', $b['timestamp'])->getTimestamp();
+
+    return $timestampB - $timestampA;
+  });
+
+  // Save the rearranged data to the JSON file
+  file_put_contents($jsonFile, json_encode($jsonData, JSON_PRETTY_PRINT));
 
   // Return a success response
   http_response_code(200);
--- makeblog/store_blog.php
+++ makeblog/store_blog.php
@@ -5,9 +5,17 @@ $blogs = [];
 if (file_exists('../json/blogs.json')) {
     $blogs = json_decode(file_get_contents('../json/blogs.json'), true);
 }
+
 array_unshift($blogs, $blogData); // Add the new blog data to the beginning of the array
 
-file_put_contents('../json/blogs.json', json_encode($blogs));
+usort($blogs, function($a, $b) {
+    $timestampA = DateTime::createFromFormat('d/m/Y, H:i:s', $a['timestamp'])->getTimestamp();
+    $timestampB = DateTime::createFromFormat('d/m/Y, H:i:s', $b['timestamp'])->getTimestamp();
+
+    return $timestampB - $timestampA;
+});
+
+file_put_contents('../json/blogs.json', json_encode($blogs, JSON_PRETTY_PRINT));
 
 http_response_code(200);
 ?>
